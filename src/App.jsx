19f2eac56428f45feb1d6fcff0e5c71e7594b922
sujit-mandal <sm.bright22@gmail.com>
2023-12-18 23:00:46 +0600

    Recomended card designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Hero from './components/Header/Hero'
 import Navbar from './components/Header/Navbar'
+import Recomended from './components/Recomended/Recomended'
 import Services from './components/Services/Services'
 import WhyChoose from './components/WhyChoose/WhyChoose'
 
@@ -13,6 +14,7 @@ function App() {
       <Hero/>
       <Services/>
       <WhyChoose/>
+      <Recomended/>
     </>
   )
 }
